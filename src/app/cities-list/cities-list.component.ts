7782e2ec352321cf9fdd206871df264d123d8d04
Quentin Mosselman <mosselman.quentin@gmail.com>
2018-11-05 09:48:16 +0100

    Edit indentation

--- src/app/cities-list/cities-list.component.ts
+++ src/app/cities-list/cities-list.component.ts
@@ -1,57 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-cities-list',
-  templateUrl: './cities-list.component.html',
-  styleUrls: ['./cities-list.component.css']
+  selector: "app-cities-list",
+  templateUrl: "./cities-list.component.html",
+  styleUrls: ["./cities-list.component.css"]
 })
 export class CitiesListComponent implements OnInit {
-
   cityForm = new FormGroup({
-    cityName: new FormControl('', [Validators.required]),
-    countryCode: new FormControl('', [Validators.required, Validators.maxLength(2), Validators.minLength(2)])
+    cityName: new FormControl("", [Validators.required]),
+    countryCode: new FormControl("", [
+      Validators.required,
+      Validators.maxLength(2),
+      Validators.minLength(2)
+    ])
   });
   cities = new Array();
   keys = new Array();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.cities = this.allStorage();
     this.keys = Object.keys(localStorage);
   }
 
-  onSubmit(){
+  onSubmit() {
     let myStorage = window.localStorage;
-    myStorage.setItem(this.cityForm.get('cityName').value, this.cityForm.get('countryCode').value);
+    myStorage.setItem(
+      this.cityForm.get("cityName").value,
+      this.cityForm.get("countryCode").value
+    );
     this.reload();
-    this.cityForm.get('cityName').setValue('');
-    this.cityForm.get('countryCode').setValue('');
+    this.cityForm.get("cityName").setValue("");
+    this.cityForm.get("countryCode").setValue("");
   }
 
   allStorage() {
-
     var values = [],
-        keys = Object.keys(localStorage);
-        var i = 0;
+      keys = Object.keys(localStorage);
+    var i = 0;
 
     while (i < keys.length) {
-        values.push( localStorage.getItem(keys[i]) );
-        i++;
+      values.push(localStorage.getItem(keys[i]));
+      i++;
     }
 
     return values;
   }
 
-  remove(i: number){
+  remove(i: number) {
     localStorage.removeItem(this.keys[i]);
     this.reload();
   }
 
-  reload(){
+  reload() {
     this.cities = this.allStorage();
     this.keys = Object.keys(localStorage);
   }
-
 }
